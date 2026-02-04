8ca2fd7bba902d64be48f5100b90784957c7d81d
minghinshi <mhshi9768@gmail.com>
2026-02-04 11:44:18 +0800

    Make ESLint ignore files in .gitignore

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,17 @@
 // @ts-check
+/* global URL */
 
 import eslint from "@eslint/js";
 import { defineConfig } from "eslint/config";
+import { includeIgnoreFile } from "@eslint/compat";
+import { fileURLToPath } from "node:url";
 import tseslint from "typescript-eslint";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
 
+const gitignorePath = fileURLToPath(new URL(".gitignore", import.meta.url));
+
 export default defineConfig(
+  includeIgnoreFile(gitignorePath),
   eslint.configs.recommended,
   tseslint.configs.strict,
   tseslint.configs.stylistic,
