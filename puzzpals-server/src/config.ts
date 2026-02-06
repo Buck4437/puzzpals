a93e7c5ed43483d00038597dd844f92c87652261
minghinshi <mhshi9768@gmail.com>
2026-02-06 13:33:06 +0800

    Assert .env variables exist

--- puzzpals-server/src/config.ts
+++ puzzpals-server/src/config.ts
@@ -1,9 +1,21 @@
 // Load environment variables
 import "dotenv/config";
 
+function assertEnvVariableExists(
+  variable: string | undefined,
+  name: string,
+): asserts variable is string {
+  if (variable === undefined) {
+    throw new Error(`${name} is missing in .env`);
+  }
+}
+
+assertEnvVariableExists(process.env.PORT, "PORT");
+assertEnvVariableExists(process.env.CLIENT_BASE_URL, "CLIENT_BASE_URL");
+
 const env = {
-  PORT: process.env.PORT || "3000",
-  CLIENT_BASE_URL: process.env.CLIENT_BASE_URL || "http://localhost:5173",
+  PORT: process.env.PORT,
+  CLIENT_BASE_URL: process.env.CLIENT_BASE_URL,
 };
 
 export default env;
