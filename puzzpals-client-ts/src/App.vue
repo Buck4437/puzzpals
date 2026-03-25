bd71145eff6e1bce5246a47b94d1529068c77cf7
buck4437 <51261544+Buck4437@users.noreply.github.com>
2026-03-25 16:42:14 +0800

    Improve ui

--- puzzpals-client-ts/src/App.vue
+++ puzzpals-client-ts/src/App.vue
@@ -8,7 +8,10 @@
       <button class="login-btn" @click="goToLogin">Login</button>
     </div>
   </header>
-  <main class="main-page">
+  <main
+    class="main-page"
+    :class="{ 'main-page-no-header': $route.meta.hideHeader === true }"
+  >
     <!-- Side Bar -->
     <NavigationSidebar
       v-if="$route.meta.hideHeader !== true"
@@ -21,6 +24,7 @@
     <CreateRoomDialog
       v-if="showCreateRoomDialog"
       @close="showCreateRoomDialog = false"
+      @upload-success="showCreateRoomDialog = false"
     />
   </main>
 </template>
@@ -30,6 +34,7 @@ import "./assets/main.css";
 import { ref } from "vue";
 import { useRouter } from "vue-router";
 import NavigationSidebar from "./components/NavigationSidebar.vue";
+import CreateRoomDialog from "./components/CreateRoomModal.vue";
 
 const router = useRouter();
 const showCreateRoomDialog = ref(false);
@@ -66,6 +71,7 @@ h1 {
   font-size: 24px;
   margin: 0;
 }
+
 main {
   height: 100%;
   width: 100%;
@@ -92,15 +98,23 @@ main {
   cursor: pointer;
   transition: box-shadow 0.2s;
 }
+
 .login-btn:hover {
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
 }
+
 .main-page {
   display: flex;
   height: 100%;
 }
+
+.main-page-no-header {
+  margin-top: 0;
+}
+
 .content-area {
   flex: 1;
-  padding: 20px;
+  height: 100%;
+  overflow-y: scroll;
 }
 </style>
