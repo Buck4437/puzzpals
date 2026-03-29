7c0ff0b3dc7452845ef24d2abaeab941fe8f1918
buck4437 <51261544+Buck4437@users.noreply.github.com>
2026-03-29 14:23:31 +0800

    Prevent room joining if it doesn't exist

--- puzzpals-client-ts/src/views/RoomPage.vue
+++ puzzpals-client-ts/src/views/RoomPage.vue
@@ -99,9 +99,11 @@ async function checkRoomExists() {
     // Check that the room exists
     const res = await api.get(`/rooms/${props.token}/exists`);
     if (res.data.exists === false) router.push("/404");
+    return res.data.exists;
   } catch (err) {
     console.error(err);
     router.push("/");
+    return false;
   }
 }
 
@@ -212,7 +214,10 @@ function initiateSocket() {
 onBeforeMount(initiateSocket);
 
 onMounted(async () => {
-  await checkRoomExists();
+  const roomExists = await checkRoomExists();
+  if (!roomExists) {
+    return;
+  }
   console.log(`Joining room ${props.token}`);
   await joinRoom();
 });
