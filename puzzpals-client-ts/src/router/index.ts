3057ecfcc38255c05771acaf368fceb2e89fc4f1
minghinshi <mhshi9768@gmail.com>
2026-04-01 14:45:25 +0800

    Redirect unknown routes to 404

--- puzzpals-client-ts/src/router/index.ts
+++ puzzpals-client-ts/src/router/index.ts
@@ -5,20 +5,38 @@ import NotFound from "@/views/NotFound.vue";
 import RoomPage from "@/views/RoomPage.vue";
 import config from "@/config";
 import EditorPage from "@/views/EditorPage.vue";
+import Catalogue from "@/views/Catalogue.vue";
 
 const router = createRouter({
   history: createWebHistory(config.baseUrl),
   routes: [
-    { path: "/", component: Home },
-    { path: "/404", component: NotFound, meta: { fullScreen: true } },
+    {
+      path: "/",
+      component: Home,
+    },
+    {
+      path: "/404",
+      component: NotFound,
+      meta: { fullScreen: true },
+    },
     {
       path: "/room/:token",
       component: RoomPage,
       props: true,
       meta: { fullScreen: true },
     },
-    { path: "/editor", component: EditorPage },
-    { path: "/catalogue", component: () => import("@/views/Catalogue.vue") },
+    {
+      path: "/editor",
+      component: EditorPage,
+    },
+    {
+      path: "/catalogue",
+      component: Catalogue,
+    },
+    {
+      path: "/:pathMatch(.*)*",
+      redirect: "/404",
+    },
   ],
 });
 
