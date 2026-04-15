ea6096c3c278dd4fcdd91635b73fbd8bfecbc39e
minghinshi <mhshi9768@gmail.com>
2026-04-15 14:32:23 +0800

    Set cookie SameSite to strict

--- puzzpals-server/src/app.ts
+++ puzzpals-server/src/app.ts
@@ -51,7 +51,7 @@ app.use(
     cookie: {
       httpOnly: true,
       secure: isProduction,
-      sameSite: isProduction ? "none" : "lax",
+      sameSite: "strict",
       maxAge: 1000 * 60 * 60 * 24 * 7,
     },
   }),
