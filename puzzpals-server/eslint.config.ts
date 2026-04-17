68cf68f72ebfa7b79b071cf396bc84e1a884dbb2
minghinshi <mhshi9768@gmail.com>
2026-04-17 09:51:18 +0800

    Add Vitest ESLint plugin to client and server

--- puzzpals-server/eslint.config.ts
+++ puzzpals-server/eslint.config.ts
@@ -1,11 +1,16 @@
 import nodeConfig from "eslint-plugin-n";
 import { defineConfig } from "eslint/config";
+import pluginVitest from "@vitest/eslint-plugin";
 
 import puzzpalsConfig from "../eslint.config.js";
 
 export default defineConfig(
   puzzpalsConfig,
   nodeConfig.configs["flat/recommended-module"],
+  {
+    ...pluginVitest.configs.recommended,
+    files: ["src/**/__tests__/*"],
+  },
   {
     languageOptions: {
       parserOptions: {
